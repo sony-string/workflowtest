4cc44dbe4a5afe7ac415acf5b74d0de83861f6de
Sa Jaeheon <jls15900@gmail.com>
2025-02-18 16:16:40 +0900

    feat(Note.java, User.java) : 노트와 사용자의 엔티티 구현

---
+++ src/main/java/moanote/backend/entity/Note.java
@@ -0,0 +1,23 @@
+package moanote.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String content;
+}
