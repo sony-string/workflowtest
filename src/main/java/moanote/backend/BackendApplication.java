cdb4f9fc45299cbbd2ee5ccdfb61c71e207c0f2e
Sa Jaeheon <jls15900@gmail.com>
2025-02-18 16:24:08 +0900

    fix(BackendApplication.java) : 스프링 앱의 데이터베이스 접근이 차단되던 문제를 해결

--- src/main/java/moanote/backend/BackendApplication.java
+++ src/main/java/moanote/backend/BackendApplication.java
@@ -2,13 +2,13 @@ package moanote.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "moanote.backend.repository")
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
