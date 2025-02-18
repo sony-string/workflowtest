3b7532cc270f79e948e78fe471b0facfcf2c1965
Sa Jaeheon <jls15900@gmail.com>
2025-02-18 16:23:22 +0900

    feat(UserService.java, NoteService.java) : 노트와 사용자의 비즈니스 로직 구현

---
+++ src/main/java/moanote/backend/service/NoteService.java
@@ -0,0 +1,22 @@
+package moanote.backend.service;
+
+import moanote.backend.entity.Note;
+import moanote.backend.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoteService {
+    @Autowired
+    private NoteRepository noteRepository;
+
+    public Note createNote(Note note) {
+        return noteRepository.save(note);
+    }
+
+    public List<Note> getNotesByUserId(Long userId) {
+        return noteRepository.findByUserId(userId);
+    }
+}
