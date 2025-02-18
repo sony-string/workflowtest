20ddc7293042cc2f36b6c9752dfbd6089001cb68
Sa Jaeheon <jls15900@gmail.com>
2025-02-18 16:22:49 +0900

    feat(NoteRepository.java, UserRepository.java) : 노트와 사용자의 DB 접근 구현

---
+++ src/main/java/moanote/backend/repository/NoteRepository.java
@@ -0,0 +1,10 @@
+package moanote.backend.repository;
+
+import moanote.backend.entity.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findByUserId(Long userId);
+}
