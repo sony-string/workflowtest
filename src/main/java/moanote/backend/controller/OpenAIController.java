915bf53e81927bd812dac53a10b87de340fc8e98
Sa Jaeheon <jls15900@gmail.com>
2025-02-20 15:58:59 +0900

    refactor(OpenAIController.java) : 오픈 AI API 서버 주소와 키를 application.properties에 숨김

--- src/main/java/moanote/backend/controller/OpenAIController.java
+++ src/main/java/moanote/backend/controller/OpenAIController.java
@@ -1,5 +1,6 @@
 package moanote.backend.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -8,9 +9,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api") // 엔드포인트 경로 정의
 public class OpenAIController {
+    @Value("${openai.api.url}")
+    private String OPENAI_API_URL;
 
-    private final String OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
-    private final String OPENAI_API_KEY = "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";
+    @Value("${openai.api.key}")
+    private String OPENAI_API_KEY;
 
     @PostMapping("/proxy")
     public ResponseEntity<?> proxy(@RequestBody Map<String, Object> requestData) {
