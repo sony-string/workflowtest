5210c863890cca23801694b698d6e9a2462004f6
SONY-STRING <sctoa1@gmail.com>
2025-03-26 23:53:55 +0900

    feat(Collaborative Editing): 다양한 데이터 타입에 대해 State-based CRDT 로직으로 동기화가 가능한 LWWRegister 클래스 구현 (ast-Write-Wins) #19
    - 상태 기반 CRDT 설계를 위한 LWWRegister 클래스 추가.
    - 다양한 데이터 타입에 대해 동기화할 수 있음

---
+++ src/main/java/moanote/backend/domain/LWWRegister.java
@@ -0,0 +1,64 @@
+package moanote.backend.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import java.util.Optional;
+
+/**
+ * LWWRegister 은 Last-Write-Wins Register 의 약자로, 가장 최근에 업데이트된 값을 가지는 레지스터를 의미한다.
+ * State-based CRDT 중 하나이다.
+ * @param <T> 동기화 할 데이터 타입. Record 이어야 한다.
+ */
+@Getter
+public class LWWRegister<T extends Record> {
+
+  /**
+   * stateId 는 timeStamp 가 같을 때, 두 레지스터의 값을 비교할 때 사용된다.
+   * Tie-breaker 로 사용된다.
+   */
+  private String stateId;
+
+  /**
+   * 논리 시계로, 레지스터의 업데이트 시간을 나타낸다.
+   */
+  private int timeStamp;
+
+  @Getter(AccessLevel.NONE)
+  private T value;
+
+  LWWRegister(String stateId, int timeStamp, T value) {
+    this.stateId = stateId;
+    this.timeStamp = timeStamp;
+    this.value = value;
+  }
+
+  LWWRegister(String stateId) {
+    this.stateId = stateId;
+    this.timeStamp = 0;
+    this.value = null;
+  }
+
+  /**
+   * 다른 레지스터와 병합한다.
+   * @param other 병합할 레지스터
+   * @return 다른 레지스터가 더 최신이면 true, 아니면 false
+   */
+  public boolean merge(LWWRegister<T> other) {
+    String othersStateId = other.getStateId();
+    int othersTimeStamp = other.getTimeStamp();
+    T othersValue = other.getValue().orElse(null);
+
+    if (othersTimeStamp > this.timeStamp || (othersTimeStamp == this.timeStamp
+        && othersStateId.compareTo(this.stateId) > 0)) {
+      this.stateId = othersStateId;
+      this.timeStamp = othersTimeStamp;
+      this.value = othersValue;
+      return true;
+    }
+    return false;
+  }
+
+  public Optional<T> getValue() {
+    return Optional.ofNullable(value);
+  }
+}
